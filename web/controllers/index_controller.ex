3497e7202eeae8935e5d3076e31c4691f977f619
EC2 Default User <ec2-user@brng.us>
2015-07-27 15:14:37 +0000

    changed rout and setup index controller

--- web/controllers/index_controller.ex
+++ web/controllers/index_controller.ex
@@ -1,12 +1,21 @@
 defmodule Phorechat.IndexController do
   use Phorechat.Web, :controller
-
+  require Logger
   alias Phorechat.Index
 
   plug :scrub_params, "index" when action in [:create, :update]
 
-  def index(conn, _params) do
+  def index(conn, params) do
+    Logger.info inspect( params,pretty: true)
+    case params do
+      %{"username" => username} ->  
+        Logger.info("username found for: " <> username)
+      _ -> redirect(conn, to: "/newuser") |> halt
+    end
     render(conn, "index.html")
+  end
+  def newuser(conn,_params) do
+    render(conn,_params)
   end
 
 end
--- web/router.ex
+++ web/router.ex
@@ -16,6 +16,7 @@ defmodule Phorechat.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+    get "/chat", IndexController, :index
   end
 
   # Other scopes may use custom stacks.
